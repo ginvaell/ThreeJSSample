04e2810a4e3a4e4e12ae339d2a52d3f7c2432cb0
Natalia Bragina <Natalia_Bragina@epam.com>
2016-10-16 19:45:12 +0300

    morphing

--- app/scripts/lab1_41_6.js
+++ app/scripts/lab1_41_6.js
@@ -9,15 +9,30 @@ export default class {
     this.renderer.setSize( window.innerWidth, window.innerHeight );
     document.body.appendChild( this.renderer.domElement );
 
-    this.tetrahedronGeomertry = new THREE.TetrahedronGeometry(40, 0);
+    this.cubeGeomertry = new THREE.BoxGeometry(40, 40, 40);
     this.sphereGeometry = new THREE.SphereGeometry(40, 10, 10);
     this.newSphereGeometry = new THREE.SphereGeometry(40, 10, 10);
+
+    for (var i = 0; i < this.newSphereGeometry.vertices.length; i++) {
+      var min = Infinity;
+      var minJ = 0;
+      for (var j = 0; j < 8; j++) {
+        if (this.newSphereGeometry.vertices[i].distanceTo(this.cubeGeomertry.vertices[j]) < min ) {
+          min = this.newSphereGeometry.vertices[i].distanceTo(this.cubeGeomertry.vertices[j]);
+          minJ = j;
+        }
+      }
+
+      this.newSphereGeometry.vertices[i] = this.cubeGeomertry.vertices[minJ];
+    }
+
+    this.sphereGeometry.morphTargets.push( { name: "target", vertices: this.newSphereGeometry.vertices } );
 
     this.redMaterial = new THREE.MeshBasicMaterial({
       wireframe: true,
       color: '#ff0000'
     });
-    this.whiteMaterial = new THREE.MeshBasicMaterial({
+    this.whiteMaterial = new THREE.MeshNormalMaterial({
       wireframe: true,
       morphTargets: true
     });
@@ -26,30 +41,28 @@ export default class {
       wireframe: true
     });
 
-    this.tetrahedron = new THREE.Mesh( this.tetrahedronGeomertry, this.redMaterial);
+    this.tetrahedron = new THREE.Mesh( this.cubeGeomertry, this.redMaterial);
     this.sphere = new THREE.Mesh( this.sphereGeometry, this.whiteMaterial);
 
-    this.scene.add( this.tetrahedron );
+    // this.scene.add( this.tetrahedron );
     this.scene.add( this.sphere );
 
     this.camera.position.z = 100;
+    this.camera.position.x = 50;
+    this.camera.position.y = 50;
+    this.camera.rotation.x = - Math.PI/6 ;
+    this.camera.rotation.y = + Math.PI/6;
 
-    for (var i = 0; i < this.newSphereGeometry.vertices.length; i++) {
-      this.newSphereGeometry.vertices[i] = this.tetrahedronGeomertry.vertices[i % 4];
-    }
-
-    console.dir(this.newSphereGeometry);
-
-    this.sphereGeometry.morphTargets.push( { name: "target", vertices: this.newSphereGeometry.vertices } );
     this.morph = 0;
   }
 
 
   render () {
-    // requestAnimationFrame( () => this.render() );
-    this.morph = 1;
-    console.dir(this.sphere.morphTargetInfluences);
-    this.sphere.morphTargetInfluences[0] = 1;
+    requestAnimationFrame( () => this.render() );
+    this.morph += 0.005;
+    if (this.morph >= 1) this.morph = 1;
+    this.sphere.morphTargetInfluences[0] = this.morph;
+    // this.camera.lookAt ( new THREE.Vector3(0, -50, 0) );
     this.renderer.render( this.scene, this.camera );
   };
 
